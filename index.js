31dfbc977baa1f849879c6834c77cd09a1adc369
alexander-8989 <alexander.m@appwrk.com>
2022-05-06 16:44:38 +0530

    Finsih with local storage and all validation

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function myValidate(){
+function myValidate() {
 
     let from = document.getElementById('from').value;
     let to = document.getElementById('to').value;
@@ -7,44 +7,78 @@ function myValidate(){
     let category = document.getElementById('category').value;
 
 
-    if(from == ""){
+    if (from == "") {
         document.getElementById('arrive').innerHTML = "please fill this field";
         return false;
-    } else if(from !== ""){
+    } else if (from !== "") {
         document.getElementById('arrive').innerHTML = '';
     }
 
-    if(date == ""){
+    if (date == "") {
         document.getElementById('datetime').innerHTML = "please choose the date";
         return false;
-    } else if(date !== ""){
+    } else if (date !== "") {
         document.getElementById('datetime').innerHTML = '';
     }
 
-    if(to == ""){
+    if (to == "") {
         document.getElementById('destinate').innerHTML = "please fill this field";
         return false;
-    } else if(to !== ""){
+    } else if (to !== "") {
         document.getElementById('destinate').innerHTML = '';
     }
 
-    if(classes == ""){
+    if (classes == "") {
         document.getElementById('myClass').innerHTML = "please fill this field";
         return false;
-    } else if(classes !== ""){
+    } else if (classes !== "") {
         document.getElementById('myClass').innerHTML = '';
     }
 
-    if(category == ""){
+    if (category == "") {
         document.getElementById('type').innerHTML = "please fill this field";
         return false;
-    } else if(category !== ""){
+    } else if (category !== "") {
         document.getElementById('type').innerHTML = '';
     }
 
-     /* validation for redirect the page */
-     window.location.href = "/table.html";
+    if(from === to){
+        document.getElementById('result').innerHTML = "origin and destination can't be same"
+        return false;
+    }else if(from !== to){
+        document.getElementById('result').innerHTML = '';
+    }
+
+
+    /* local storage validation*/
+    let bookTicket = {
+        'From': from.trim(),
+        'To': to.trim(),
+        'Date': date,
+        'Classes': classes,
+        'Category': category,
+    };
+
+    let existingEntries = JSON.parse(localStorage.getItem('bookTicket'));
+    if (existingEntries == null) {
+        existingEntries = [];
+    }
+
+    localStorage.setItem('bookTicket', JSON.stringify(bookTicket));
+    existingEntries.unshift(bookTicket);
+    localStorage.setItem('bookTicket', JSON.stringify(existingEntries));
+
+
+    /* validation for redirect the page */
+    window.location.href = "/table.html";
 
     /* validation for reset the form*/
     document.getElementById("reset").click();
+
+}
+
+function swapping() {
+    var temp = document.getElementById('from').value;
+    document.getElementById('from').value = document.getElementById('to').value;
+    document.getElementById('to').value = temp;
 }
